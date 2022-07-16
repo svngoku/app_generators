591f8273649104a60b578e314ba7995577946074
marymarty104@gmail.com <marymarty104@gmail.com>
2022-07-16 17:31:32 +0200

    adding the go server generator

--- service_generator.ts
+++ service_generator.ts
@@ -54,6 +54,80 @@ const generate = (schema: Schema, { scope }: Config) => {
   };
 };
 
+const go_server_generator = (schema: Schema, { scope}): Config => {
+    const { ref, refs, model, models, singleParam } = buildNameVariations(schema);
+    const go_template = `
+        package main
+
+        import (
+        	"fmt"
+        	"html/template"
+        	"log"
+        	"net/http"
+        )
+
+        func ${model}(w http.ResponseWriter, r *http.Request) {
+        	fmt.Println("${model}!")
+        }
+
+        type ${model} struct {
+            //
+        }
+
+        //PageVariables are variables sent to the html template
+       
+
+        var todos []Todo
+
+        func getTodos(w http.ResponseWriter, r *http.Request) {
+        	pageVariables := PageVariables{
+        		PageTitle: "Get Todos",
+        		PageTodos: todos,
+        	}
+        	t, err := template.ParseFiles("todos.html")
+
+        	if err != nil {
+        		http.Error(w, err.Error(), http.StatusBadRequest)
+        		log.Print("Template parsing error:", err)
+        	}
+
+        	err = t.Execute(w, pageVariables)
+        }
+
+        func add${model}(w http.ResponseWriter, r *http.Request) {
+        	err := r.ParseForm()
+        	if err != nil {
+        		http.Error(w, err.Error(), http.StatusBadRequest)
+        		log.Print("Request parsing error: ", err)
+        	}
+
+        	todo := Todo{
+        		Title:   r.FormValue("title"),
+        		Content: r.FormValue("content"),
+        	}
+
+        	todos = append(todos, todo)
+        	log.Print(todos)
+        	http.Redirect(w, r, "/todos/", http.StatusSeeOther)
+        }
+
+        func main() {
+        	http.HandleFunc("/", home)
+        	http.HandleFunc("/todos/", getTodos)
+        	http.HandleFunc("/add-todo/", addTodo)
+        	fmt.Println("Server is running on port :8080")
+        	log.Fatal(http.ListenAndServe(":8080", nil))
+
+        }
+    `;
+
+    return {
+        go_template,
+        title: `${models} Server`,
+        fileName: `${refs}/${refs}.server.go`,
+      };
+};
+
 export const ServiceGenerator: Generator = {
-  generate,
+  generate
 };
